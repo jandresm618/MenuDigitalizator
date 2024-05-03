3ac042c0cfa32f78915d798165f82eb6abdc2162
Mosquera Merchan Javier Andres <javier.mosquera@aizoon.local>
2024-05-03 16:45:44 +0200

    Image 2 added.

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ import os
 
 project_dir = os.getcwd()
 image_path = os.path.join(project_dir, 'Images', 'MenuExample.png')
-img = cv2.imread(image_path,0)
+image_path2 = os.path.join(project_dir, 'Images', 'duetorre.png')
+img = cv2.imread(image_path2,0)
+print(img.shape)
 #imgshow(img,"Imagen original")
 
 #kernel = cv2.getGaussianKernel((5, 5), 2)
 #print(kernel)
-imagen_suavizada = cv2.GaussianBlur(img, (3, 3), 2)
+kernel = (3, 3)
+imagen_suavizada = cv2.GaussianBlur(img, kernel, 2)
 imgshow(imagen_suavizada,"Imagen con filtro Gaussiano")
 
-kernel=np.ones((3, 3), np.uint8)
+kernel=np.ones(kernel, np.uint8)
 erode=img.copy()
 
 for i in range(0, 4):
     
     dilate = cv2.dilate(erode, kernel)
     a=("Erosionado{"+str(i+1)+"} times")
-    muestra(a,dilate)
+    imgshow(dilate,a)
 
 
 
